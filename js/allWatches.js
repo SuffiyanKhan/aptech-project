396becc3e41eb8e6c88872ea9335c68c636d9549
muhammadaffan187 <affankhank246@gmail.com>
2024-07-18 22:26:20 +0500

    working

--- js/allWatches.js
+++ js/allWatches.js
@@ -5,11 +5,11 @@ console.log(productName)
 document.querySelector("#heading").innerHTML = productName
 
 fetch("../js/allWatchesData.json")
-    .then(res => res.json())
-    .then(async (data) => {
-        const filteredData = data.allWatchData.filter(watch => watch.Gender === productName);
-        await datarender(filteredData)
-    })
+  .then(res => res.json())
+  .then(async (data) => {
+    const filteredData = data.allWatchData.filter(watch => watch.Gender === productName);
+    await datarender(filteredData)
+  })
 // let datarender = async (data) => {
 //     try {
 //         let allData = await data
@@ -35,13 +35,13 @@ fetch("../js/allWatchesData.json")
 
 
 let datarender = async (data) => {
-    try {
-        let allData = await data;
-        allData.forEach(element => {
-            console.log(element.thumbnail[0]);
-            console.log(element);
-            document.querySelector("#data").innerHTML += `
-            <div class="col-lg-3 col-md-4 col-sm-6">
+  try {
+    let allData = await data;
+    allData.forEach(element => {
+      console.log(element.thumbnail[0]);
+      console.log(element);
+      document.querySelector("#data").innerHTML += `
+            <div class="col-lg-3 col-md-4 col-sm-6"onclick="detailPage('${element.id}')">
           <div class="card"
             style="box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;border: none;cursor: pointer;">
             <img src="${element.image}" class="card-img-top" alt="Placeholder Image"
@@ -51,15 +51,18 @@ let datarender = async (data) => {
                 ${element.Brand}</h5>
               <p class="card-text" style="font-size:14px ;">${element.title}</p>
               <p class="card-text" style="font-size:14px ;">${element.discountPrice
-              }</p>
+        }</p>
             </div>
           </div>
         </div>
             
             `;
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
+const detailPage = (id) => {
+  alert(id)
+}
