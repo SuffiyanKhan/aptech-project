5e4ca7529c86437872321e17d38a96d11bd8472d
Suffiyan Khan <suffiyanahmed804092@gmai.com>
2024-07-19 18:46:32 +0500

    working

--- js/navbar.js
+++ js/navbar.js
@@ -1,28 +1,46 @@
 document.querySelector("#navbar").innerHTML = `
- <nav class="navbar px-3 navbar-expand-lg bg-body-tertiary sticky-top">
-        <div class="container-fluid">
-          <a class="navbar-brand d-flex align-items-center" href="#">
-            <img src="./images/logo.png" width="100" alt="">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+<nav class="navbar px-3 navbar-expand-lg bg-white sticky-top"
+style="box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;">
+<div class="container-fluid">
+    <a class="navbar-brand d-flex align-items-center" href="../index.html">
+        <img src="./images/logo.png" width="100" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+            <li class="nav-item">
                 <a class="nav-link fw-bold active" aria-current="page" href="../index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link fw-bold" href="#">About</a>
-              </li>
-              <li class="nav-item">
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                   Watch Category
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="../pages/saleWatchesDetailPage.html?name=Mens Watches">Mens Watches</a></li>
+                    <li><a class="dropdown-item" href="../pages/saleWatchesDetailPage.html?name=Ladies Watches">Ladies Watches</a></li>
+                    <li><a class="dropdown-item" href="../pages/saleWatchesDetailPage.html?name=Pre-Owned Watches">Pre-Owned Watches</a></li>
+                    <li><a class="dropdown-item" href="../pages/saleWatchesDetailPage.html?name=Diving Watches">Diving Watches</a></li>
+                    <li><a class="dropdown-item" href="../pages/saleWatchesDetailPage.html?name=Limited Edition Watches">Limited Edition Watches</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link fw-bold" href="#">Contact us</a>
-              </li>
-            </ul>
-           
-          </div>
+            </li>
+        </ul>
+        <div class="">
+            <button class="btn rounded-5" style="border: none;">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </button>
         </div>
-      </nav>
+
+    </div>
+</div>
+</nav>
 `
 
 const navLinks = document.querySelectorAll('.nav-link');
--- js/shopCard.js
+++ js/shopCard.js
@@ -1,8 +1,6 @@
 fetch("./js/cardsData.json")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-
         data.ShopData.forEach(item => {
             document.querySelector("#data").innerHTML += `
         <div class="card" style=" width: 16rem;border: none;cursor: pointer;" onclick="detailPage('${item.name}')">
@@ -35,3 +33,36 @@ let detailPage = (name) => {
     const encodedName = encodeURIComponent(name);
     window.location.href = `../pages/shopdetailpage.html?name=${encodedName}`
 }
+
+
+// branded
+
+fetch("./js/allWatchesData.json")
+    .then(res => res.json())
+    .then(async(data) => {
+        const filteredData = data.allWatchData.filter(watch => watch.Gender === "Branded");
+        await datarender(filteredData)
+    })
+    .catch(error => console.error(error.message))
+
+
+    const datarender=(data)=>{
+        data.forEach(item => {
+            document.querySelector("#branded").innerHTML += `
+        <div class="card" style=" width: 16rem;border: none;cursor: pointer;" onclick="detailPage('${item.name}')">
+            <img src="${item.image}" class="card-img-top" style="background-color: #fafafa; "
+                alt="...">
+            <div class="card-body">
+                <p class="card-title fw-semibold" style="font-size: 14px;">${item.
+                    name
+                }</p>
+                <p class="card-text" style="font-size: 14px;">${truncateTitle(item.title, 4)}</p>
+                <div class="d-flex align-items-center">
+                    <h5 class="card-text" style="color: #a0142c;">${item.discountPrice}</h5>
+                    <small class="card-text ms-2 text-secondary"> <del>${item.price}</del></small>
+                </div>
+            </div>
+        </div>
+        `
+        });
+    }
